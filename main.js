310dc26e2ab30d32c793bffdf5468ba3bfdd1241
Kazuya HATTORI <k_hattori@ap-com.co.jp>
2017-12-27 13:11:03 +0900

    つくった

---
+++ main.js
@@ -0,0 +1,26 @@
+$(function() {
+  var stampCard = $('.stamp-card');
+  var createSquare = function (d, stamp, current) {
+    var item = $('<div>').addClass('item');
+    if (current) {
+      item.addClass('current');
+    }
+    item.append($('<span>').addClass('day').text(d));
+    if (stamp) {
+      item.append($('<img>').attr('src', 'stamp.png'));
+    }
+    stampCard.append(item);
+  };
+  $.ajax({
+    url: 'data.json',
+    type: 'GET',
+  })
+  .done(function(data) {
+    var days = data.days;
+    for(var d = 1; d <= days; d++) {
+      var current = $.inArray(d, data.currents) != -1;
+      var stamp = current || $.inArray(d, data.stamps) != -1;
+      createSquare(d, stamp, current);
+    }
+  });
+});
